c4224b6bc0b0750483d33391a6c214a7344ad435
mhashirhassan22 <mhashirhassan22@gmail.com>
2024-08-26 07:34:13 +0400

    add pagination in list articles

--- app/api/routes/articles.py
+++ app/api/routes/articles.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from app.schemas.article import Article, ArticleCreate
 from app.services.article_service import create_article
 from app.deps import SessionDep
@@ -10,7 +10,7 @@ router = APIRouter()
 
 @router.post("/", response_model=Article)
 def create_new_article(article: ArticleCreate, db: SessionDep):
-    existing_article = db.query(Article).filter(Article.title == article.title).first()
+    existing_article = db.query(ArticleModel).filter(ArticleModel.title == article.title).first()
     if existing_article:
         raise HTTPException(status_code=400, detail="Article with this title already exists")
 
@@ -18,6 +18,10 @@ def create_new_article(article: ArticleCreate, db: SessionDep):
     return db_article
 
 @router.get("/", response_model=List[Article])
-def list_articles(db: SessionDep):
-    articles = db.exec(select(ArticleModel)).all()
+def list_articles(
+    db: SessionDep,
+    skip: int = Query(0, ge=0, description="Number of items to skip"),
+    limit: int = Query(10, ge=1, le=100, description="Max number of items to return")
+):
+    articles = db.exec(select(ArticleModel).offset(skip).limit(limit)).all()
     return articles
