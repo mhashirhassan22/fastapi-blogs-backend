c10f8abd37b3fdafc8bcdb7128d69b6a97d18298
mhashirhassan22 <mhashirhassan22@gmail.com>
2024-08-26 08:53:42 +0400

    update from_orm to model_validate

--- app/services/article_service.py
+++ app/services/article_service.py
@@ -3,7 +3,7 @@ from app.models.article import Article
 from app.schemas.article import ArticleCreate
 
 def create_article(db: Session, article_create: ArticleCreate) -> Article:
-    new_article = Article.from_orm(article_create)
+    new_article = Article.model_validate(article_create)
     db.add(new_article)
     db.commit()
     db.refresh(new_article) # auto gen id
